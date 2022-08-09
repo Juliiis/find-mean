8c92ab9701d3427be4e3a8441fbc5486055f9002
Julieta Martin <julieta.martin@clarity.ai>
2022-08-09 12:16:23 +0200

    test and logic part done

--- app/src/main/java/gradle/java/FindMean.java
+++ app/src/main/java/gradle/java/FindMean.java
@@ -1,4 +1,12 @@
 package gradle.java;
 
+
 public class FindMean {
+    public static int findmean(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum / nums.length;
+    }
 }
--- app/src/test/java/gradle/java/FindMeanTest.java
+++ app/src/test/java/gradle/java/FindMeanTest.java
@@ -1,4 +1,17 @@
 package gradle.java;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FindMeanTest {
+    @Test
+    public void mean_expected_to_be_four() {
+        assertEquals(4, FindMean.findmean(new int[]{1,3,5,7}));
+    }
+
+    @Test
+    public void mean_expected_to_be_twelve() {
+        assertEquals(6, FindMean.findmean(new int[]{6,6}));
+    }
 }
