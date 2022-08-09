f4cf115b24f933eb8644636b6f5137215cb62632
Julieta Martin <julieta.martin@clarity.ai>
2022-08-09 13:44:56 +0200

    refactor the sintax test

--- app/src/test/java/gradle/java/FindMeanTest.java
+++ app/src/test/java/gradle/java/FindMeanTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FindMeanTest {
     @Test
     public void mean_expected_to_be_four() {
-        assertEquals(4, FindMean.findmean(new int[]{1,3,5,7}));
+        int[] numbers = new int[]{1,3,5,7};
+        assertEquals(4, FindMean.findmean(numbers));
     }
 
     @Test
     public void mean_expected_to_be_twelve() {
-        assertEquals(6, FindMean.findmean(new int[]{6,6}));
+        int[] numbers = new int[]{6,6};
+        assertEquals(6, FindMean.findmean(numbers));
     }
 }
